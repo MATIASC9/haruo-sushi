31f0f8d97c16232025fe54494fce2830bddf4bba
Matias Carrizo <matias-carrizo@outlook.com>
2022-06-09 19:46:27 -0300

    seccion pedidos estilizada

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // VARIABLES
 const iconoMenu = document.querySelector('#iconoMenu');
 const menuOpciones = document.querySelector('#menuOpciones');
+const slideImagenes = document.querySelector('#slideImagenes');
 
 iconoMenu.addEventListener('click', () => {
-    menuOpciones.classList.toggle('--mostrar');
+  menuOpciones.classList.toggle('--mostrar');
 }
 );
+// Funcion para cambiar imagenes con un timeOut
+const cambiarImagenes = (i) => {
+  let url;
+  setTimeout(() => {
+    url = `../resources/img/sushi-${i}.webp`;
+    slideImagenes.style.background = "no-repeat center/cover url(" + url + ")";
+  }, 2000 * i);
+};
+for (let i = 1; i < 4; i++) {
+  cambiarImagenes(i);
+}
